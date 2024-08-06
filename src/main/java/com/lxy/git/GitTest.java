c2d34bbc08b6f5dad4ec0e8028878eed45ebb836
lxy <lxy16384@126.com>
2024-08-06 09:56:10 +0800

    third commit

--- src/main/java/com/lxy/git/GitTest.java
+++ src/main/java/com/lxy/git/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git_test___111");
         System.out.println("git_test___222");
+        System.out.println("git_test___333");
 
     }
 }
