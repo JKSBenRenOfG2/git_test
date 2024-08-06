1c9baaabbbc34a3970b712ed92783638c66d68d2
lxy <lxy16384@126.com>
2024-08-06 09:48:35 +0800

    first commit

---
+++ src/main/java/com/lxy/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.lxy.git;
+
+/**
+ * ClassName: GitTest
+ * Package: com.lxy.git
+ *
+ * @Author lxy
+ * @Create 2024/8/6 9:42
+ * @Version 1.0
+ * Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git_test___111");
+    }
+}
