99745a10a92b22d8acec3d51fdc2490df39d008d
lxy <lxy16384@126.com>
2024-08-06 09:55:54 +0800

    second commit

--- src/main/java/com/lxy/git/GitTest.java
+++ src/main/java/com/lxy/git/GitTest.java
@@ -12,5 +12,7 @@ package com.lxy.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git_test___111");
+        System.out.println("git_test___222");
+
     }
 }
